765e58b2c2f85640859799e74c4495d800588164
Andre Guedes <andregamoraes94@gmail.com>
2025-08-30 02:57:01 -0300

    Add prettier and eslint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,25 +1,22 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
+/** @type {import('eslint').Linter.FlatConfig[]} */
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+	...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:prettier/recommended'),
+	{
+		ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'dist/**', 'coverage/**', 'next-env.d.ts'],
+		rules: {
+			'prettier/prettier': ['error', { tabWidth: 4 }]
+		}
+	}
 ];
-
-export default eslintConfig;
